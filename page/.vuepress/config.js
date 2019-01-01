414a99430574d642417f3d66d495c61005a8f5d0
mikakane <t.goto@chatbox-inc.com>
2019-01-01 20:53:19 +0900

    vuepress 化

---
+++ page/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: '実践Webコーディング',
+    description: 'HTML/CSS を使った Web 制作技術について、現場で使えるテクニックを紹介します。',
+    head: [
+        ['script', { src: "https://static.codepen.io/assets/embed/ei.js"}]
+    ],
+    markdown: {
+        anchor: {
+            level: [1,2,3],
+            slugify: (s) => encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-')),
+            permalink: true,
+            permalinkBefore: true,
+            permalinkSymbol: '#'
+        }
+    },
+    themeConfig: {
+        nav: [
+            { text: 'Lec Café', link: 'https://leccafe.connpass.com/' },
+        ],
+        sidebar: [
+            '/1.コンテナとレイアウト.md',
+            '/2.レスポンシブデザイン.md',
+            '/3.クラス名の管理.md',
+        ],
+    }
+}
