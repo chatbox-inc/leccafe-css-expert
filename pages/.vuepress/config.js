be0822454018a2faf8b3e06f6ec7398531e1b497
mikakane <t.goto@chatbox-inc.com>
2019-01-03 16:46:10 +0900

    update

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    // theme: "lec-cafe",
+    theme: "lec-cafe",
     title: '実践Webコーディング',
     description: 'HTML/CSS を使った Web 制作技術について、現場で使えるテクニックを紹介します。',
     head: [
         ['script', { src: "https://static.codepen.io/assets/embed/ei.js"}]
     ],
+    locales: {
+        '/': {
+            lang: 'ja',
+        },
+    },
     markdown: {
         anchor: {
             level: [1,2,3],
